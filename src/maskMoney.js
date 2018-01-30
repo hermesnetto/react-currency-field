d5f21604787f4195b4b9d50a2eeaa6ea155d5aca
Rubens Ramalho <rubens.ramalho.neto@gmail.com>
2018-01-30 00:08:33 -0200

    Update maskMoney

--- src/maskMoney.js
+++ src/maskMoney.js
@@ -15,10 +15,13 @@ export default class MaskMoney {
     },
   };
 
-  OPTION;
-
   constructor (locale = 'pt-BR', option = {}) {
-    
+    this.option = {
+      charThousands: '.',
+      charDecimal: ',',
+      symbol: 'R$',
+      decimalScale: 2
+    };
   }
 
 }
@@ -30,14 +33,39 @@ const options = {
   decimalScale: 2
 };
 
-const stringToNumber = (str) => {
+const locale = 'en-US';
+
+const locales = {
+  'pt-BR': {
+    charThousands: '.',
+    charDecimal: ',',
+    symbol: 'R$',
+    decimalScale: 2
+  },
+  'en-US': {
+    charThousands: ',',
+    charDecimal: '.',
+    symbol: 'US$',
+    decimalScale: 2
+  },
+};
+
+const isNumber = num => Number(num) === num;
+
+const clearString = (str) => {
   return str.replace(/[\D]+/g, '');
 };
 
-const numberToString = (num) => {
-  const floatNumber = `${num.substr(0, num.length - locales[locale].decimalScale)}.${num.substr(locales[locale].decimalScale * (-1))}`;
-  let floatValue = floatNumber.split('.');
+export const numberToString = (num) => {
+  const numberFormat = isNumber(num) ? clearString(num.toFixed(locales[locale].decimalScale)) : clearString(num);
+  const strMoney = `${numberFormat.substr(0, numberFormat.length - locales[locale].decimalScale)}.${numberFormat.substr(locales[locale].decimalScale * (-1))}`;
+  let floatValue = strMoney.split('.');
   
   floatValue[0] = floatValue[0].split(/(?=(?:...)*$)/).join(locales[locale].charThousands);
-  return floatValue.join(locales[locale].charDecimal);
+  return `${locales[locale].symbol} ${floatValue.join(locales[locale].charDecimal)}`;
+};
+
+export const stringToNumber = (str) => {
+  const strFormat = clearString(str);
+  return parseFloat(`${strFormat.substr(0, strFormat.length - locales[locale].decimalScale)}.${strFormat.substr(locales[locale].decimalScale * (-1))}`);
 };
