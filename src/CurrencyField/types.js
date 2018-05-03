7b3f844c41aece67eeb65923dc91efda32fd33ba
Hermes Netto <dev.hermes@hotmail.com>
2018-05-03 10:41:57 -0300

    Add flow, tests and some improvements

---
+++ src/CurrencyField/types.js
@@ -0,0 +1,30 @@
+// @flow
+import * as React from 'react';
+
+export type Values = {
+  floatValue: number,
+  formatedValue: string,
+};
+
+export type Locales = 'pt-BR' | 'en-US';
+
+export type LocaleConfig = {
+  charThousands: string,
+  charDecimal: string,
+  symbol: string,
+  decimalScale: number,
+};
+
+export type Props = {
+  value: string | number,
+  name?: string,
+  onChange?: (e: SyntheticEvent<HTMLInputElement>, values: Values) => void,
+  onFocus?: (e: SyntheticEvent<HTMLInputElement>) => void,
+  onBlur?: (e: SyntheticEvent<HTMLInputElement>) => void,
+  className?: string,
+  placeholder?: string,
+  disabled?: boolean,
+  required?: boolean,
+  style?: { [string]: string | number },
+  locale: Locales | LocaleConfig,
+};
